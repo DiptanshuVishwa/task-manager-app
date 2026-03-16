a7e67157a2af70fe6bd232b7add13715b968cdb1
DiptanshuVishwa <diptanshuvishwa364@gmail.com>
2026-03-17 04:23:47 +0530

    Fix authentication cookies for production

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (
-        !origin ||
-        origin.includes("vercel.app") ||
-        origin.includes("localhost")
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://task-manager-app-one-snowy.vercel.app",
+      "https://task-manager-lva1bwtsf-diptanshu-vishwas-projects.vercel.app"
+    ],
     credentials: true
   })
 );
