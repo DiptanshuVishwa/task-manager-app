b99914b03cb5ffd97c5ece1f06087295c888dc27
DiptanshuVishwa <diptanshuvishwa364@gmail.com>
2026-03-17 03:22:38 +0530

    Fix CORS for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+    "https://task-manager-app-one-snowy.vercel.app"
+    ],
     credentials: true
   })
 );
